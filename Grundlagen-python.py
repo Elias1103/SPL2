02b1b8119769868d9798e330567ea447b2b3e713
Elias1103 <171wita29@o365.htl-leoben.at>
2018-04-23 15:39:16 +0200

    Grundlagen-python.py

--- Grundlagen-python.py
+++ Grundlagen-python.py
@@ -1,4 +1,7 @@
 # grundlagen-python.py
+import random
+siegera = 0
+siegerb = 0
 
 # Kommentare erfolgen mit hastag
 
@@ -23,3 +26,34 @@ else:
 lieblingszahl = input("Was ist deine Lieblingszahl?")
 print("Super, ich mag die Zahl ", lieblingszahl)
 print("Aber die coolere Zahl ", int(lieblingszahl)+10, "mag ich noch mehr!")
+
+runden = input("Wie viele Runden sollen wir spielen? ")
+runden = int(runden)
+
+sieger = ""
+siegerComputer = ""
+
+for runde in range(1, runden+1):
+    ## Zufallszahl erzeugen
+    Zufallszahl = random.randint(1,6)
+    ## ist die Zufallszahl 1,3 oder 5: bin ich der Gewinner
+    ## sonst ist der Computer der Gewinner
+    if (Zufallszahl == 1 or Zufallszahl == 3 or Zufallszahl == 5):
+        sieger = "ich"
+        siegera += 1
+    else:
+        sieger = "Computer"
+        siegerb += 1    
+    print("Runde" , runde, "von", runden, ": Sieger", sieger, ": gewuerfelte Zahl", Zufallszahl)
+print("Ich habe gewonnen ", siegera)
+print("Der Computer hat gewonnen" , siegerb)
+
+if(siegera <= siegerb):
+    print("Der Computer hat gewonnen")
+
+elif(siegera == siegerb):
+    print("Keiner hat gewonnen")
+else:
+    (siegerb <= siegera)
+    print("Ich habe gewonnen!")
+print("Game Over!")
